df7cc27e6abd48344188b2f9ad13aafe9782eb32
mdraselmiah95 <russelroy15@gmail.com>
2022-11-14 23:02:07 +0600

    Learn and Understand Webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/scripts/index.js",
@@ -25,12 +26,26 @@ module.exports = {
         test: /\.scss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          minimize: {
+            removeComments: false,
+            collapseWhitespace: false,
+          },
+        },
+      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./images/webpack.png",
     }),
   ],
 };
