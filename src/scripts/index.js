6706225e698552bb6daf77eb9c77e0159b2d7638
mdraselmiah95 <russelroy15@gmail.com>
2022-11-14 20:41:39 +0600

    Learn and Understand Webpack 5 setup

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import { ABC } from "./test";
+
+const fn = () => {
+  console.log("I am learning webpack.");
+};
+fn();
+
+console.log(ABC);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  mode: "development",
 };
 
 // index.js Entry point
